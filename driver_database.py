9aa50e41592ee784b78bf70ff091b3d16d63702a
Christoph Pohl <christoph.pohl1@mailbox.tu-dresden.de>
2015-12-11 16:47:02 +0100

    Dialog for adding drivers; not functional yet

--- driver_database.py
+++ driver_database.py
@@ -44,15 +44,100 @@ class DriverDatabaseFrame(QtGui.QWidget):
             ["Manufacturer", "Model", "Fs [Hz]", u"Vas [m³]", u"Sd [m²]", "Qts",
              "xmax [mm]"])
 
+        # populate table
         for driver in driver_db:
-            self.add_driver(driver)
+            self.add_driver_entry(driver)
 
+        add_driver_button = QtGui.QPushButton(self)
+        add_driver_button.setIcon(QtGui.QIcon.fromTheme('list-add'))
+        add_driver_button.setText("Add new driver")
+        add_driver_button.clicked.connect(self.add_driver)
         vbox = QtGui.QVBoxLayout()
+        vbox.addWidget(add_driver_button, stretch=0)
         vbox.addWidget(self.table_widget)
         self.setLayout(vbox)
 
-    def add_driver(self, driver):
-        """ Add a new driver to the QTableWidget """
+    def add_driver(self):
+        add_driver_dialog = QtGui.QDialog()
+
+        # Driver general specification
+        general_info = QtGui.QGroupBox("General Specification")
+        info_form = QtGui.QFormLayout()
+        info_form.setFieldGrowthPolicy(QtGui.QFormLayout.FieldsStayAtSizeHint)
+
+        manuf_label = QtGui.QLabel()
+        manuf_label.setText("Manufacturer")
+        manuf_line = QtGui.QLineEdit()
+
+        model_label = QtGui.QLabel()
+        model_label.setText("Model")
+        model_line = QtGui.QLineEdit()
+        
+        info_form.addRow(manuf_label, manuf_line)
+        info_form.addRow(model_label, model_line)
+        general_info.setLayout(info_form)
+
+        # Thiele/Small parameters
+        ts_info = QtGui.QGroupBox("Thiele/Small Parameters")
+        ts_form = QtGui.QFormLayout()
+
+        fs_label = QtGui.QLabel()
+        fs_label.setText("Resonance Frequency: fs")
+        fs_box = QtGui.QDoubleSpinBox()
+        fs_box.setSuffix(" Hz")
+        fs_box.setRange(10.0, 2e4)
+
+        Qts_label = QtGui.QLabel()
+        Qts_label.setText("Total Q of Driver at fs: Qts")
+        Qts_box = QtGui.QDoubleSpinBox()
+        Qts_box.setRange(0.0, 1.0)
+
+        Sd_label = QtGui.QLabel()
+        Sd_label.setText("Diaphragm Area: Sd")
+        Sd_box = QtGui.QDoubleSpinBox()
+        Sd_box.setSuffix(u" cm²")
+        Sd_box.setRange(0.0, 1e3)
+
+        xmax_label = QtGui.QLabel()
+        xmax_label.setText("Maximum linear peak excursion: xmax")
+        xmax_box = QtGui.QDoubleSpinBox()
+        xmax_box.setSuffix(" mm")
+        xmax_box.setRange(0.0, 20.0)
+
+        Vas_label = QtGui.QLabel()
+        Vas_label.setText("Equivalent Compliance Volume: Vas")
+        Vas_box = QtGui.QDoubleSpinBox()
+        Vas_box.setSuffix(" l")
+        Vas_box.setRange(0.0, 1e3)
+        
+        ts_form.addRow(fs_label, fs_box)
+        ts_form.addRow(Qts_label, Qts_box)
+        ts_form.addRow(Sd_label, Sd_box)
+        ts_form.addRow(xmax_label, xmax_box)
+        ts_form.addRow(Vas_label, Vas_box)
+        ts_info.setLayout(ts_form)
+
+        # Accept/cancel buttons
+        buttons_hbox = QtGui.QHBoxLayout()
+        accept_button = QtGui.QPushButton(self)
+        accept_button.setIcon(QtGui.QIcon.fromTheme('dialog-apply'))
+        accept_button.setText("Accept")
+        cancel_button = QtGui.QPushButton(self)
+        cancel_button.setIcon(QtGui.QIcon.fromTheme('gtk-close'))
+        cancel_button.setText("Cancel")
+        buttons_hbox.addWidget(cancel_button)
+        buttons_hbox.addWidget(accept_button)
+
+        # putting it together
+        vbox = QtGui.QVBoxLayout()
+        vbox.addWidget(general_info)
+        vbox.addWidget(ts_info)
+        vbox.addLayout(buttons_hbox)
+        add_driver_dialog.setLayout(vbox)
+        add_driver_dialog.exec_()
+
+    def add_driver_entry(self, driver):
+        """ Add a new driver entry to the QTableWidget """
         rows = self.table_widget.rowCount()
         self.table_widget.setRowCount(rows+1)
 
